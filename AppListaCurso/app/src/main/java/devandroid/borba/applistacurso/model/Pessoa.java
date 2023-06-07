bb4b1a0a7bfa9fc05e9233946a8c21fe7e5ef118
Lucas <lucas_borba@outlook.com>
2023-06-07 17:42:20 -0300

    Uso dos métodos Get e Set

--- AppListaCurso/app/src/main/java/devandroid/borba/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/borba/applistacurso/model/Pessoa.java
@@ -3,8 +3,47 @@ package devandroid.borba.applistacurso.model;
 public class Pessoa {
 
     //Construtor
-    public Pessoa(){
+    //Atributos - Objetos - Molde - Modelo - Template
+    // Metodos de acesso Gets and Sets
 
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    public Pessoa() {
 
     }
 }
--- AppListaCurso/app/src/main/java/devandroid/borba/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/borba/applistacurso/view/MainActivity.java
@@ -10,6 +10,10 @@ import devandroid.borba.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        //Atribuir conteúdo, dados, valores ao objeto
+        //Conforme Modelo, Template
+        pessoa.setPrimeiroNome("Lucas");
+        pessoa.setSobreNome("Borba");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("54-999444223");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Fernanda");
+        outraPessoa.setSobreNome("Cioato");
+        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setTelefoneContato("54-95544321");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+        int parada = 0;
     }
 }

e9bf9a6c467258fe1f7c23641b8709abe2474921
Lucas <lucas_borba@outlook.com>
2023-06-13 15:04:30 -0300

    SharedPreferences Limpar dados salvos

--- AppListaCurso/app/src/main/java/devandroid/borba/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/borba/applistacurso/view/MainActivity.java
@@ -17,7 +17,9 @@ import devandroid.borba.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
     public static final String NOME_PREFERENCES = "pref_listavip";
+
 
     PessoaController controller;
     Pessoa pessoa;
@@ -37,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preferences = getSharedPreferences(NOME_PREFERENCES, 0);
-        SharedPreferences.Editor listaVip = preferences.edit();
+        listaVip = preferences.edit();
 
         controller = new PessoaController();
         controller.toString();
@@ -70,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 editSobreNomeAluno.setText("");
                 editTelefoneContato.setText("");
                 editNomeCurso.setText("");
+
+                listaVip.clear();
+                listaVip.apply();
             }
         });
 

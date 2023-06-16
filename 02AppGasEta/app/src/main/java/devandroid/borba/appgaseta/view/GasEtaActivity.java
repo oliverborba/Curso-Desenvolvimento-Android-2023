2b55a278c87fd50e568ccdcd519c6e28ef7f0112
Lucas <lucas_borba@outlook.com>
2023-06-16 15:21:42 -0300

    activity_gaseta

--- 02AppGasEta/app/src/main/java/devandroid/borba/appgaseta/view/GasEtaActivity.java
+++ 02AppGasEta/app/src/main/java/devandroid/borba/appgaseta/view/GasEtaActivity.java
@@ -1,4 +1,18 @@
 package devandroid.borba.appgaseta.view;
 
-public class GasEtaActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.borba.appgaseta.R;
+
+public class GasEtaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gaseta);
+    }
 }

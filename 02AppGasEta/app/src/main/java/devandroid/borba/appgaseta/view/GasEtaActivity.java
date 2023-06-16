bc5ec2924ab50b4c00817b1f2e5a07c3d246e416
Lucas <lucas_borba@outlook.com>
2023-06-16 19:57:18 -0300

    Implementando Funcionalidade Botão Limpar

--- 02AppGasEta/app/src/main/java/devandroid/borba/appgaseta/view/GasEtaActivity.java
+++ 02AppGasEta/app/src/main/java/devandroid/borba/appgaseta/view/GasEtaActivity.java
@@ -1,6 +1,10 @@
 package devandroid.borba.appgaseta.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -11,7 +15,16 @@ import devandroid.borba.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
 
-//    UtilGasEta utilGasEta = new UtilGasEta();
+
+    EditText editGasolina;
+    EditText editEtanol;
+
+    TextView txtResultado;
+
+    Button btnCalcular;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,8 +32,46 @@ public class GasEtaActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_gaseta);
 
-//        utilGasEta.metodoEstatico();
+        editGasolina = findViewById(R.id.editGasolina);
+        editEtanol = findViewById(R.id.editEtanol);
 
+        txtResultado = findViewById(R.id.txtResultado);
+
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                editEtanol.setText("");
+                editGasolina.setText("");
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GasEtaActivity.this, "GasEta boa economia...", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
 
 
         Toast.makeText(GasEtaActivity.this, UtilGasEta.calcularMelhorOpcao(5.12, 3.99), Toast.LENGTH_LONG).show();

0249876e42413a7f2da2a2ef085db9a2d3c40a46
Lucas <lucas_borba@outlook.com>
2023-06-16 15:52:41 -0300

    Implementando metodo estático e não estático

---
+++ 02AppGasEta/app/src/main/java/devandroid/borba/appgaseta/apoio/UtilGasEta.java
@@ -0,0 +1,21 @@
+package devandroid.borba.appgaseta.apoio;
+
+public class UtilGasEta {
+
+    public void metodoEstatico() {
+
+
+    }
+
+    public static void metodoNaoEstatico() {
+
+    }
+
+    public static String mensagem() {
+        return "Qualquer mensagem...";
+    }
+
+    public static double preco() {
+        return 9.99;
+    }
+}
